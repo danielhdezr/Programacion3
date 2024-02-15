b13aa47905e8ed69675b68bae7277fcdd6034b05
danielhdezr <159938910+danielhdezr@users.noreply.github.com>
2024-02-15 10:08:06 -0700

    Ventana roja

---
+++ ProgramacionIII/src/Interfaz.java
@@ -0,0 +1,16 @@
+import java.awt.Dimension;
+
+import javax.swing.JFrame;
+
+public class Interfaz {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Ventana ventana = new Ventana();
+		
+	
+
+	}
+
+}
